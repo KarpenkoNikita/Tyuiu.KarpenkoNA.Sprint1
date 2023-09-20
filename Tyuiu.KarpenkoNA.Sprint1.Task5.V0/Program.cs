0e267126e25f21aa757573d5e90cdae34c176ea9
Tyuiu.KarpenkoNA@outlook.com <karpenko.nik05@mail.ru>
2023-09-20 17:42:49 +0500

    Task0

---
+++ Tyuiu.KarpenkoNA.Sprint1.Task5.V0/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KarpenkoNA.Sprint1.Task1.V16.Lib;
+//ЗАДАНИЕ
+// Написать программу, которая запрашивает у пользователя исходные данные,
+// вычисляет результат по формуле x*5*y+2*a и печатает его на экране.
+
+namespace Tyuiu.KarpenkoNA.Sprint1.Task1.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #0 | Выполнил: Карпенко Н.А. | АСОиУБ-23-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода\вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Карпенко Никита Андреевич |  АСОиУБ-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле x*5*y+2*a и печатает его на экране.      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double a, x, y;
+
+            Console.WriteLine("Введите значение а:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+
+        }
+       
+            
+    }
+}
